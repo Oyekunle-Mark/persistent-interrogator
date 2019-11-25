fa662676c6d281fd29dbf8781383de6481d6766c
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-25 17:32:40 +0100

    Opened the problems file

--- main.go
+++ main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
 	filename := flag.String("csv", "problems.csv", "The csv file that contains the quiz in the order 'question,answer'")
-
 	flag.Parse()
 
-	_ = filename
-	fmt.Println(*filename)
+	file, err := os.Open(*filename)
+
+	if err != nil {
+		fmt.Printf("Cannot open the file %s\n", *filename)
+		os.Exit(1)
+	}
+	_ = file
 }
